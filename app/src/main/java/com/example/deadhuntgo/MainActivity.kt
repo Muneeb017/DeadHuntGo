c88ede557a58c057a10a32cc8142cc926a2216f5
Muneeb Khalid <muneebkhalid0017@gmail.com>
2022-12-27 19:38:19 +0500

    new

--- app/src/main/java/com/example/deadhuntgo/MainActivity.kt
+++ app/src/main/java/com/example/deadhuntgo/MainActivity.kt
@@ -8,18 +8,18 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.age)
+        setContentView(R.layout.frame_14)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        val data = ArrayList<String>()
-        data.add("0-12")
-        data.add("13-19")
-        data.add("20-35")
-        data.add("36-56")
-        data.add("57-90")
-        val adapter = AgeAdapter(data)
-        recyclerView.adapter = adapter
+       // val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+       // recyclerView.layoutManager = LinearLayoutManager(this)
+       // val data = ArrayList<String>()
+       // data.add("0-12")
+       // data.add("13-19")
+       // data.add("20-35")
+       // data.add("36-56")
+       // data.add("57-90")
+       // val adapter = AgeAdapter(data)
+       // recyclerView.adapter = adapter
 
     }
 }
